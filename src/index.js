ce14c7dffb695c5300f81188ad33852c8bda52f8
Chumak Slavik <chumakslav@gmail.com>
2023-01-15 22:46:54 +0200

    js done

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { getPhotos } from './getPhotos/getPhotos';
+import throttle from 'lodash.throttle';
 
 const ITEMS_PER_PAGE = 40;
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
-  loadMore: document.querySelector('.load-more'),
 };
 
 let page = 1;
@@ -16,14 +16,15 @@ let totalHits = 0;
 let searchText;
 
 refs.searchForm.addEventListener('submit', onSearchFormSubmitHandler);
-refs.loadMore.addEventListener('click', onLoadMoreClickHandler);
+
+const throttledHandler = throttle(onWindowScrollHandler, 300);
+
 const lightbox = new SimpleLightbox('.gallery a');
 
 function onSearchFormSubmitHandler(event) {
   event.preventDefault();
   refs.gallery.innerHTML = '';
   page = 1;
-  refs.loadMore.style.display = 'none';
 
   const {
     elements: { searchQuery },
@@ -36,6 +37,7 @@ function onSearchFormSubmitHandler(event) {
   }
 
   getAndDisplayPhotos(searchText);
+  window.addEventListener('scroll', throttledHandler);
 }
 
 function getAndDisplayPhotos() {
@@ -55,9 +57,7 @@ function getAndDisplayPhotos() {
     }
 
     if (page * ITEMS_PER_PAGE < totalHits) {
-      refs.loadMore.style.display = 'flex';
     } else {
-      refs.loadMore.style.display = 'none';
       Notify.info("We're sorry, but you've reached the end of search results.");
     }
   });
@@ -101,7 +101,18 @@ function displayPhotos(photos) {
   lightbox.refresh();
 }
 
-function onLoadMoreClickHandler(event) {
-  page++;
-  getAndDisplayPhotos(searchText);
+function onWindowScrollHandler(event) {
+  const endOfPage =
+    window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+  if (endOfPage) {
+    page++;
+    if (page * ITEMS_PER_PAGE >= totalHits) {
+      removeInfiniteScroll();
+    }
+    getAndDisplayPhotos(searchText);
+  }
 }
+
+const removeInfiniteScroll = () => {
+  window.removeEventListener('scroll', throttledHandler);
+};
